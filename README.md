74e862db09a1c088c5e9753f211786c3e8df7543
rharitha97 <74331015+rharitha97@users.noreply.github.com>
2020-12-05 04:00:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class21-Arron
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,37 @@
+var fixedRect, movingRect;
+var gameobject;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = 5;
+  fixedRect.velocityY = -5;
+
+}
+
+function draw() {
+  background(0,0,0);  
+  
+  if(movingRect.x - fixedRect.x < movingRect.width/2 +  fixedRect.width/2 &&
+    fixedRect.x - movingRect.x < movingRect.width/2 +  fixedRect.width/2) {
+      movingRect.velocityX = movingRect.velocityX * (-1);
+      fixedRect.velocityX =  fixedRect.velocityX * (-1);
+  }
+  if(movingRect.y - fixedRect.y < movingRect.height/2 +  fixedRect.height/2 &&
+      fixedRect.y - movingRect.y < movingRect.height/2 +  fixedRect.height/2) {
+        movingRect.velocityY = movingRect.velocityY * (-1);
+        fixedRect.velocityY =  fixedRect.velocityY * (-1);
+  }
+  
+  drawSprites();
+}
+
+function bounceOff() {
+  
+}
+
